c55de6d435b300e843fd43ccb7fef41466d82dc9
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-08 00:40:56 +0100

    Add fun facts

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 const title = '';
 const background = <img className='background' alt='ocean' src='/img/ocean.jpg' />;
+// Display facts
+function displayFact(e) {
+    const funFact = window.animals[e.target.alt].facts[Math.floor(Math.random() * window.animals[e.target.alt].facts.length)];
+    document.getElementById('fact').innerHTML = funFact;
+}
 // Add an array of images
 const images = [];
 for (const animal in window.animals) {
-    images.push(<img key={animal} className='animal' alt={animal} src={window.animals[animal].image} aria-label={animal} role='button'/>)
+    images.push(<img key={animal} onClick={displayFact} className='animal' alt={animal} src={window.animals[animal].image} aria-label={animal} role='button'/>)
 }
 const animalFacts = (
     <div>
         {background}
+        <p id='fact'></p>
         <div className='animals'>
             {images}
         </div>
