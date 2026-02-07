d3dbf5c4b19fe94532c3f6090e06960e094bcedb
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-08 00:18:23 +0100

    Add array of images

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 const title = '';
 const background = <img className='background' alt='ocean' src='/img/ocean.jpg' />;
+// Add an array of images
+const images = [];
+for (const animal in window.animals) {
+    images.push(<img key={animal} className='animal' alt={animal} src={window.animals[animal].image} aria-label={animal} role='button'/>)
+}
 const animalFacts = (
     <div>
         {background}
+        <div className='animals'>
+            {images}
+        </div>
         <h1>{title ? title : 'Click an animal for a fun fact'}</h1>
     </div>
 );
