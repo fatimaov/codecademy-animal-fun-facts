08c1bf445f91b6d14af8d44cd21ebd79ce0c6dde
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-08 00:04:05 +0100

    Add background

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
 const title = '';
-const animalFacts = <h1>{title ? title : 'Click an animal for a fun fact'}</h1>;
+const background = <img className='background' alt='ocean' src='/img/ocean.jpg' />;
+const animalFacts = (
+    <div>
+        {background}
+        <h1>{title ? title : 'Click an animal for a fun fact'}</h1>
+    </div>
+);
 root.render(animalFacts);
